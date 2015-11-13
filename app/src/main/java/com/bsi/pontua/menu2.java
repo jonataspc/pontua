7506f2f2244551550523766d2276ebd5d65aa3d4
Jonatas <jonataspc@gmail.com>
2015-11-13 03:13:06 -0200

    Evoluc desenv

--- app/src/main/java/com/bsi/pontua/menu2.java
+++ app/src/main/java/com/bsi/pontua/menu2.java
@@ -67,6 +67,19 @@ public class Menu2 extends AppCompatActivity {
 
         });
 
+        ((Button) findViewById(R.id.btnCadastroItensInspecao)).setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(Menu2.this, CadastroItensInspecao.class);
+                Bundle b = new Bundle();
+                //b.putString("usuario", edtUsuario.getText().toString().trim());
+                myIntent.putExtras(b);
+                startActivity(myIntent);
+            }
+
+        });
+
 /*
 
         View myView = findViewById(R.id.button);
