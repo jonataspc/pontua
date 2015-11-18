900a4ccbf72b04ae78e795a88773e70278746cdc
Jonatas <jonataspc@gmail.com>
2015-11-18 02:02:48 -0200

    login via rest

--- app/src/main/java/com/bsi/pontua/Login.java
+++ app/src/main/java/com/bsi/pontua/Login.java
@@ -18,6 +18,68 @@ import java.util.List;
 import controle.CadastrosControle;
 import vo.UsuarioVO;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.params.HttpParams;
+import org.json.JSONObject;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.params.HttpParams;
+import org.json.JSONObject;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.Toast;
+
+
 //TODO LIST
 //Bug - atraves de lanc. por NFC permite pontuar entidades que nao sejam do evento cadastrado
 
@@ -29,6 +91,9 @@ public class Login extends AppCompatActivity {
     EditText edtUsuario;
     EditText edtSenha;
     ProgressDialog progress;
+
+    private static String SERVICE_URL=null;
+
 
     public static Context getContextOfApplication() {
         return contextOfApplication;
@@ -133,7 +198,16 @@ public class Login extends AppCompatActivity {
         editor.commit();
 
 
-        try {
+
+
+        //login rest
+        String httphost=((EditText) findViewById(R.id.edtServidor)).getText().toString().replace(":3307","");
+
+        SERVICE_URL = "http://" + httphost + ":8080/PontuaWeb/api/v1/login";
+
+        postData();
+
+  /*      try {
 
             String[] paramns = new String[]{edtUsuario.getText().toString().trim(), edtSenha.getText().toString().trim()};
 
@@ -145,10 +219,255 @@ public class Login extends AppCompatActivity {
         } catch (Exception e) {
             e.printStackTrace();
             Toast.makeText(getApplicationContext(), "Erro: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+        }*/
+
+    }
+
+
+
+    public void postData() {
+
+        WebServiceTask wst = new WebServiceTask(WebServiceTask.POST_TASK, this, "Posting data...");
+
+
+        final EditText edtUsuario = (EditText) findViewById(R.id.edtUsuario);
+        final EditText edtSenha = (EditText) findViewById(R.id.edtSenha);
+
+
+        wst.addNameValuePair("nome", edtUsuario.getText().toString().trim());
+        wst.addNameValuePair("senha",  edtSenha.getText().toString().trim());
+
+
+        // the passed String is the URL we will POST to
+        wst.execute(new String[] { SERVICE_URL });
+
+    }
+
+
+    public void handleResponse(String response) {
+
+
+        try {
+
+            JSONObject jso = new JSONObject(response);
+
+            String nome = jso.getString("firstName");
+
+
+        } catch (Exception e) {
+            Log.e("", e.getLocalizedMessage(), e);
         }
 
     }
 
+
+    private class WebServiceTask extends AsyncTask<String, Integer, String> {
+
+        public static final int POST_TASK = 1;
+        public static final int GET_TASK = 2;
+
+        private static final String TAG = "PontuaTag";
+
+        // connection timeout, in milliseconds (waiting to connect)
+        private static final int CONN_TIMEOUT = 3000;
+
+        // socket timeout, in milliseconds (waiting for data)
+        private static final int SOCKET_TIMEOUT = 5000;
+
+        private int taskType = GET_TASK;
+        private Context mContext = null;
+        private String processMessage = "Processing...";
+
+        private ArrayList<NameValuePair> params = new ArrayList<NameValuePair>();
+
+        private ProgressDialog pDlg = null;
+
+        public WebServiceTask(int taskType, Context mContext, String processMessage) {
+
+            this.taskType = taskType;
+            this.mContext = mContext;
+            this.processMessage = processMessage;
+        }
+
+        public void addNameValuePair(String name, String value) {
+
+            params.add(new BasicNameValuePair(name, value));
+        }
+
+
+
+        @Override
+        protected void onPreExecute() {
+            inicializaProgressBar();
+            progress.show();
+        }
+
+
+        private  String convertInputStreamToString(InputStream inputStream) throws IOException{
+            BufferedReader bufferedReader = new BufferedReader( new InputStreamReader(inputStream));
+            String line = "";
+            String result = "";
+            while((line = bufferedReader.readLine()) != null)
+                result += line;
+
+            inputStream.close();
+            return result;
+
+        }
+
+        protected String doInBackground(String... urls) {
+
+            String url = urls[0];
+            String result = "";
+
+            HttpResponse response = doResponse(url);
+
+
+
+           if (response == null) {
+                return result;
+            } else {
+
+                try {
+                    InputStream inputStream = null;
+
+                    if(response.getEntity()==null){
+                        return null;
+                    }
+
+                    inputStream = response.getEntity().getContent();
+
+                    result = convertInputStreamToString(inputStream);
+
+                } catch (IllegalStateException e) {
+                    Log.e(TAG, e.getLocalizedMessage(), e);
+
+                } catch (IOException e) {
+                    Log.e(TAG, e.getLocalizedMessage(), e);
+                }
+
+            }
+
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(String response) {
+
+            handleResponse(response);
+
+
+
+            pDlg.dismiss();
+
+        }
+
+        // Establish connection and socket (data retrieval) timeouts
+        private HttpParams getHttpParams() {
+
+            HttpParams htpp = new BasicHttpParams();
+
+            HttpConnectionParams.setConnectionTimeout(htpp, CONN_TIMEOUT);
+            HttpConnectionParams.setSoTimeout(htpp, SOCKET_TIMEOUT);
+
+            return htpp;
+        }
+
+        private HttpResponse doResponse(String url) {
+
+            // Use our connection and data timeouts as parameters for our
+            // DefaultHttpClient
+            HttpClient httpclient = new DefaultHttpClient(getHttpParams());
+
+            HttpResponse response = null;
+
+            try {
+                switch (taskType) {
+
+                    case POST_TASK:
+                        HttpPost httppost = new HttpPost(url);
+                        // Add parameters
+                        //httppost.setEntity(new UrlEncodedFormEntity(params));
+
+                        String json = "";
+
+                        // 3. build jsonObject
+                        JSONObject jsonObject = new JSONObject();
+
+                        for(NameValuePair x: params){
+                            jsonObject.accumulate(x.getName(),  x.getValue() );
+                        }
+
+                        //jsonObject.accumulate("name", person.getName());
+                        //jsonObject.accumulate("country", person.getCountry());
+                        //jsonObject.accumulate("twitter", person.getTwitter());
+
+                        // 4. convert JSONObject to JSON to String
+                        json = jsonObject.toString();
+
+                        // ** Alternative way to convert Person object to JSON string usin Jackson Lib
+                        // ObjectMapper mapper = new ObjectMapper();
+                        // json = mapper.writeValueAsString(person);
+
+                        // 5. set json to StringEntity
+                        StringEntity se = new StringEntity(json);
+
+                        // 6. set httpPost Entity
+                        httppost.setEntity(se);
+
+                        // 7. Set some headers to inform server about the type of the content
+                        httppost.setHeader("Accept", "application/json");
+                        httppost.setHeader("Content-type", "application/json");
+
+
+
+
+
+                        response = httpclient.execute(httppost);
+
+
+                        break;
+                    case GET_TASK:
+                        HttpGet httpget = new HttpGet(url);
+                        response = httpclient.execute(httpget);
+                        break;
+                }
+            } catch (Exception e) {
+
+                Log.e(TAG, e.getLocalizedMessage(), e);
+
+            }
+
+            return response;
+        }
+
+        private String inputStreamToString(InputStream is) {
+
+            String line = "";
+            StringBuilder total = new StringBuilder();
+
+            // Wrap a BufferedReader around the InputStream
+            BufferedReader rd = new BufferedReader(new InputStreamReader(is));
+
+            try {
+                // Read response until the end
+                while ((line = rd.readLine()) != null) {
+                    total.append(line);
+                }
+            } catch (IOException e) {
+                Log.e(TAG, e.getLocalizedMessage(), e);
+            }
+
+            // Return full string
+            return total.toString();
+        }
+
+    }
+
+
+
+
+/*
 
     class validarLoginTask extends AsyncTask<String, Integer, UsuarioVO> {
 
@@ -241,6 +560,7 @@ public class Login extends AppCompatActivity {
 
 
     }
+*/
 
 
 }
