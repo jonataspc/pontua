83811ec2e40af8f2a1b6f856547e4f80244c1cb7
Jonatas Cruz <jonataspc@gmail.com>
2016-10-30 23:49:14 -0200

    Ajuste em Sobre

--- app/src/main/java/com/bsi/pontua/Menu3.java
+++ app/src/main/java/com/bsi/pontua/Menu3.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -63,8 +64,17 @@ public class Menu3 extends AppCompatActivity {
 
 
                 AlertDialog.Builder alert = new AlertDialog.Builder(context);
-                alert.setTitle("Sobre"); //Set Alert dialog title here
-                alert.setMessage("Permitido o uso sem finalidade comercial\n\nVersão: " + versionName);
+                alert.setTitle("Pontua!"); //Set Alert dialog title here
+                alert.setMessage("Projeto desenvolvido para o Trabalho de Conclusão de Curso dos alunos Daniel Silva, Flávio Proche, João Paulo e Jônatas Cruz para o curso de BSI das Faculdades Integradas "  +
+                        "Santa Cruz.\n\nVersão: " + versionName + "\n\n" +
+                        "Copyright 2016 Equipe DFJJ\n" +
+                        "\n" +
+                        "Licensed under the Apache License, Version 2.0 (the “License”); you may not use this file except in compliance with the License. You may obtain a copy of the License at\n" +
+                        "\n" +
+                        "http://www.apache.org/licenses/LICENSE-2.0\n" +
+                        "\n" +
+                        "Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an “AS IS” BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.");
+
                 alert.setPositiveButton("OK", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int whichButton) {
 
@@ -73,6 +83,10 @@ public class Menu3 extends AppCompatActivity {
                 });
                 AlertDialog alertDialog = alert.create();
                 alertDialog.show();
+
+                // Must call show() prior to fetching views
+                TextView messageView = (TextView)alertDialog.findViewById(android.R.id.message);
+                messageView.setGravity(Gravity.CENTER_VERTICAL + Gravity.LEFT);
 
                 return true;
 
